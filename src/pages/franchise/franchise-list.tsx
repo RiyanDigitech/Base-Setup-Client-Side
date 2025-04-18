a68919ed9d6294722249c00cde96436da53ceb1f
HabibQureshiDigitech <habib@digitechinfra.com>
2025-04-18 11:01:50 +0500

    chore:franchise(color change)

--- src/pages/franchise/franchise-list.tsx
+++ src/pages/franchise/franchise-list.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function FranchiseList() {
-  return <div>FranchiseList</div>;
+  return <div className='bg-red-500'>FranchiseList</div>;
 }
 
 export default FranchiseList;
