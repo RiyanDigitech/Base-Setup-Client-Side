83bbb20c3ef690fd9ce0944f68a29f78d37a1db0
furqandigitech <furqan@digitechinfra.com>
2025-04-29 19:00:00 +0500

    chores:submenu(nested)

--- src/lib/types/menu.ts
+++ src/lib/types/menu.ts
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 
+
 export interface SubMenu {
   id: number;
   name: string;
@@ -13,6 +14,7 @@ export interface Menu {
   type: string;
   key: number;
   children: SubMenu[];
+  subMenus?: Menu[];
 }
 
 export interface EditItem {
@@ -36,3 +38,42 @@ export interface Props {
   editModeType: "submenu" | "menu";
   setEditModeType: Dispatch<SetStateAction<"submenu" | "menu">>;
 }
+
+
+// export interface SubMenu {
+//   id: number;
+//   name: string;
+//   key: number;
+//   type: string;
+//   children?: SubMenu[]; 
+// }
+
+// export interface Menu {
+//   id: number;
+//   name: string;
+//   type: string;
+//   key: number;
+//   children: SubMenu[];
+// }
+
+// export interface EditItem {
+//   id: number;
+//   name: string;
+//   menuId: number;
+// }
+
+// export interface Props {
+//   draftMenus: Menu[];
+//   setDraftMenus: Dispatch<SetStateAction<Menu[]>>;
+//   showDraftEditModal: (menu: Menu) => void;
+//   deleteDraftMenu: (id: number) => void;
+//   showSubMenuModal: (id: number) => void;
+//   showContentModal: (id: number) => void;
+//   saveDraftToMenus: () => void;
+//   isEditModalOpen: boolean;
+//   setIsEditModalOpen: Dispatch<SetStateAction<boolean>>;
+//   editItem: EditItem;
+//   setEditItem: Dispatch<SetStateAction<EditItem>>;
+//   editModeType: "submenu" | "menu";
+//   setEditModeType: Dispatch<SetStateAction<"submenu" | "menu">>;
+// }
